a9447b435bd264859d898d356676921326dda8dd
Deser7 <deser7@icloud.com>
2025-08-08 15:47:20 +0400

    создал исходный проект

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Наташа Спиридонова on 08.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
