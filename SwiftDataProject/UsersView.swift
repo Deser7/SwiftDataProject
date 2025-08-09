3417b9d206c2250c9377efeab4420dbc66ae1a0c
Deser7 <deser7@icloud.com>
2025-08-09 18:40:32 +0400

    добавил взаимодействие моделей между собой

--- SwiftDataProject/UsersView.swift
+++ SwiftDataProject/UsersView.swift
@@ -9,18 +9,47 @@ import SwiftData
 import SwiftUI
 
 struct UsersView: View {
+    @Environment(\.modelContext) var modelContext
     @Query var users: [User]
     
     var body: some View {
         List(users) { user in
-            Text(user.name)
+            HStack {
+                Text(user.name)
+                
+                Spacer()
+                
+                Text(String(user.jobs.count))
+                    .fontWeight(.black)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .clipShape(.capsule)
+            }
         }
+        .onAppear(perform: addSample)
     }
     
     init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]) {
         _users = Query(filter: #Predicate<User> { user in
             user.joinDate >= minimumJoinDate
         }, sort: \User.name)
+    }
+    
+    func addSample() {
+        let userOne = User(
+            name: "Piper Chapman",
+            city: "New York",
+            joinDate: .now
+        )
+        let jobOne = Job(name: "Organize sock drawer", priority: 3)
+        let jobTwo = Job(name: "Make plans with Alex", priority: 4)
+        
+        modelContext.insert(userOne)
+        
+        userOne.jobs.append(jobOne)
+        userOne.jobs.append(jobTwo)
     }
 }
 
